5430f096d669c651db6d9677282772151d769ec2
Nakod55 <nakodvenkatesh43@gmail.com>
2024-03-18 00:58:51 +0530

    submit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const {readFile}=require("fs");
 
 // Write a GET route to return the value stored in the NUMBER variable stored in the .env file
 /*
@@ -10,6 +11,10 @@ app.use(express.json());
 */
 app.get('/api/get-env', (req, res) => {
    //Write your code here
+   readFile("./.env",(err,data)=>{
+          const num=data.toString()[7];
+          res.send({number: num});
+   })
 });
 
 module.exports = app;
